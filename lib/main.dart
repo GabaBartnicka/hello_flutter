9496a8e79af8cd812ac97f87bc35abd78d859903
Gaba Bartnicka <bartnicka.g@gmail.com>
2021-01-03 12:57:23 +0100

    Displaying image

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text('Hello World'),));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hi!'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.black12,
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://images.pexels.com/photos/6334707/pexels-photo-6334707.jpeg?cs=srgb&dl=pexels-roman-odintsov-6334707.jpg&fm=jpg'),
+          ),
+        )
+      ),
+    ),
+  );
 }
